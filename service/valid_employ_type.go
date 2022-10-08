06dccba98e2938293b8c1af1e45380efb06295f2
SKKUGoon <54009920+SKKUGoon@users.noreply.github.com>
2022-10-09 02:10:30 +0900

    check validity of the contract period

--- service/valid_employ_type.go
+++ service/valid_employ_type.go
@@ -1,1 +1,17 @@
 package service
+
+import (
+	"errors"
+	"fmt"
+)
+
+func ValidateContractPeriod(period int, empType string) error {
+	switch {
+	case period <= 0 && empType != "permanent":
+		return errors.New(fmt.Sprintf("non-positive period %v, empType should be permanent", period))
+	case period > 0 && empType == "permanent":
+		return errors.New(fmt.Sprintf("positive work period %v, empType should be contract", period))
+	default:
+		return nil
+	}
+}
