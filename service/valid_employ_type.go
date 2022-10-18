f7434e1165bb3d6fe38d4cbbde1776224ffab37e
SKKUGoon <54009920+SKKUGoon@users.noreply.github.com>
2022-10-18 23:53:09 +0900

    -

--- service/valid_employ_type.go
+++ service/valid_employ_type.go
@@ -1,22 +1,2 @@
 package service
 
-import (
-	"errors"
-	"fmt"
-)
-
-// ValidateContractPeriod
-//
-//	period is <= 0 then worker is considered a permanent worker
-//	period is > 0 then worker is considered a contract worker.
-//	period should be integer.
-func ValidateContractPeriod(period int, empType string) error {
-	switch {
-	case period <= 0 && empType != "permanent":
-		return errors.New(fmt.Sprintf("non-positive period %v, empType should be permanent", period))
-	case period > 0 && empType == "permanent":
-		return errors.New(fmt.Sprintf("positive work period %v, empType should be contract", period))
-	default:
-		return nil
-	}
-}
