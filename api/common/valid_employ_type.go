c4cf6f9fada7e1fe735f889cad6a3a19a1f24319
SKKUGoon <54009920+SKKUGoon@users.noreply.github.com>
2023-01-05 21:20:49 +0900

    add month validation method

--- api/common/valid_employ_type.go
+++ api/common/valid_employ_type.go
@@ -1,6 +1,9 @@
 package common
 
-import "errors"
+import (
+	"errors"
+	"time"
+)
 
 func ValidateEmployType(value string) error {
 	possible := map[string]bool{
@@ -21,6 +24,16 @@ func ValidatePayFreq(value string) error {
 	}
 	if !possible[value] {
 		return errors.New("no such pay frequency")
+	}
+	return nil
+}
+
+func ValidateMonthDate(day time.Time, value string) error {
+	if value != "M" {
+		return nil
+	}
+	if day.Day() > 25 {
+		return errors.New("payday with interval `M` should not be later than 25th")
 	}
 	return nil
 }
