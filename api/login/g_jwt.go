4e06c7be30daaa3226bbb905d8acf234ed29163a
SKKUGoon <54009920+SKKUGoon@users.noreply.github.com>
2023-02-12 18:50:28 +0900

    util printout whenever JWT Token is created

--- api/login/g_jwt.go
+++ api/login/g_jwt.go
@@ -2,7 +2,9 @@ package api_login
 
 import (
 	"errors"
+	"fmt"
 	"github.com/golang-jwt/jwt/v4"
+	"griffin-dao/util"
 	"os"
 	"time"
 )
@@ -28,5 +30,8 @@ func createJWTToken(userinfo string) (string, error) {
 	if err != nil {
 		return "", errors.New("token generation error")
 	}
+	// Notice to server log
+	m := fmt.Sprintf("New JWT generated. Expiring at %v", expire)
+	util.PrintYellowStatus(m)
 	return tks, nil
 }
